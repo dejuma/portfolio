ee28849d30072758d0bd875c637357d9c89471fc
joe <josepholesin@hotmail.com>
2023-08-04 22:35:22 +0100

    refactoring again

--- src/components/BrowserProgress/BrowserProgess.jsx
+++ src/components/BrowserProgress/BrowserProgess.jsx
@@ -5,7 +5,7 @@ const BrowserProgess = () => {
     <div className="flex items-center justify-center h-screen bg-gray-900">
       <div className="p-8 bg-white bg-opacity-85 rounded-lg shadow-md text-center">
         <h2 className="text-2xl font-semibold text-black mb-4">
-          The Website is in progress
+          The Website is in currently progress
         </h2>
         <p className="text-gray-600">
           We&apos;re currently working on this webste. Please check back later!
