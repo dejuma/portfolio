8db8bbb076661f005b84d77194e68f82b2c0d09d
joe <josepholesin@hotmail.com>
2023-09-05 02:06:26 +0100

    removing unused components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import SocialLink from "../Shared/SocialLink";
 import contactImg from "/public/assets/contact.png";
 
