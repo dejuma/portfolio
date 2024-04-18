1e6154e89807b4c9f2572c20e5abaad841dd92a4
Joe <josepholesin@hotmail.com>
2024-04-18 10:27:09 +0100

    some scaling issues fixed

--- src/components/Shared/SocialLink.jsx
+++ src/components/Shared/SocialLink.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const SocialLink = ({ href, icon, label }) => {
   return (
     <Link href={href} aria-label={label}>
-      <div className="rounded-full shadow-lg shadow-gray-400 p-6 bg-[#e4e7ed] text-[#1f2937] cursor-pointer hover:scale-110 ease-in duration-300">
+      <div className="rounded-full shadow-lg shadow-gray-400 p-4 bg-[#e4e7ed] text-[#1f2937] cursor-pointer hover:scale-110 ease-in duration-300">
         {icon}
       </div>
     </Link>
